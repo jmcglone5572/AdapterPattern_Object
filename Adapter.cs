29f6404388001d4a19d0d648e0c8a3e4cffca699
Jim McGlone <jmcglone5572@sbcglobal.net>
2019-07-11 09:15:20 -0700

    Initial Revision

---
+++ Adapter.cs
@@ -0,0 +1,19 @@
+﻿namespace AdapterPattern_Object
+{
+    public class Adapter : IExpectedInterface
+    {
+        private TargetClass target;
+
+        public Adapter(TargetClass target)
+        {
+
+        }
+
+        public void MethodA()
+        {
+            target.MethodB();
+        }
+
+
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AdapterPattern_Object
+{
+    class Program
+    {
+        static IExpectedInterface dependency = new Adapter(new TargetClass());
+
+        static void Main(string[] args)
+        {
+            dependency.MethodA();
+        }
+    }
+}
